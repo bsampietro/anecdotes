ca155396e69286e72116d86aecfd1fe3932a4866
bruno2 <bsampietro@gmail.com>
2009-05-12 11:43:21 -0300

    added some post validations, change style in user register div

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,9 +7,9 @@ class Post < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :text
   
-  #validates_length_of :text, :minimum => xx :maximum => xx
-  #validates_inclusion_of :tag_id, :in => %w(1 2 3)
-  #validates_inclusion_of :language_code, :in => %w(1 2 3)
+  #validates_length_of :title, :minimum => MIN_POST_TITLE_LETTERS, :maximum => MAX_POST_TITLE_LETTERS
+  #validates_length_of :text, :minimum => MIN_POST_TEXT_LETTERS :maximum => MAX_POST_TEXT_LETTERS
+  validates_inclusion_of :language_code, :in => %w(wc en es)
   
   attr_accessible :title, :text, :tag_id, :language_code
   
@@ -47,6 +47,12 @@ class Post < ActiveRecord::Base
     end
     
     {:order => order, :conditions => conditions}
+  end
+  
+  protected
+  
+  def validate    
+    self.errors.add(:tag_id) if !self.tag
   end
 
 end
